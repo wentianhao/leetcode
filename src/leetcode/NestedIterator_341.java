7eeaea043a1c2c322dd98418a7039ce1de7daa8b
wenhao <1024008684@qq.com>
2021-03-23 17:15:50 +0800

    扁平化嵌套列表迭代器

---
+++ src/leetcode/NestedIterator_341.java
@@ -0,0 +1,60 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class NestedIterator_341 {
+
+      // This is the interface that allows for creating nested lists.
+      // You should not implement it, or speculate about its implementation
+     public interface NestedInteger {
+
+          // @return true if this NestedInteger holds a single integer, rather than a nested list.
+         public boolean isInteger();
+
+         // @return the single integer that this NestedInteger holds, if it holds a single integer
+          // Return null if this NestedInteger holds a nested list
+         public Integer getInteger();
+
+         // @return the nested list that this NestedInteger holds, if it holds a nested list
+          // Return null if this NestedInteger holds a single integer
+          public List<NestedInteger> getList();
+      }
+
+    public class NestedIterator implements Iterator<Integer> {
+
+        private List<Integer> res = new ArrayList<>();
+        private int i;
+
+        private void init(List<NestedInteger> nestedList){
+            for(NestedInteger n : nestedList){
+                if (n.isInteger()){
+                    res.add(n.getInteger());
+                }else{
+                    init(n.getList());
+                }
+            }
+        }
+
+        public NestedIterator(List<NestedInteger> nestedList) {
+            init(nestedList);
+        }
+
+        @Override
+        public Integer next() {
+            return res.get(i++);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return i < res.size();
+        }
+    }
+
+/*
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
+}
